33b79f13a58ed173279b05639617d254786a8cdd
niche0905 <yudle537@gmail.com>
2021-10-20 21:07:11 +0900

    적 파일 수정

--- enemy.py
+++ enemy.py
@@ -57,17 +57,17 @@ class Goomba:
     # def death_check(self):
     #     self.death = True
 
-def handle_events():
-    global running
-
-    events = get_events()
-
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
+# def handle_events():
+#     global running
+#
+#     events = get_events()
+#
+#     for event in events:
+#         if event.type == SDL_QUIT:
+#             running = False
+#         elif event.type == SDL_KEYDOWN:
+#             if event.key == SDLK_ESCAPE:
+#                 running = False
 
 # open_canvas(WIDTH, HEIGHT)
 
