5b423f016f74ddd4b9bdfaca774445f3c5dfeb44
niche0905 <yudle537@gmail.com>
2021-10-20 20:28:17 +0900

    마리오 객체 나눔

---
+++ mario.py
@@ -0,0 +1,80 @@
+from pico2d import *
+import math
+
+g = 10
+
+class Mario:
+    def __init__(self):
+        self.image = load_image('mario2.png')
+        self.x, self.y = 400, 300
+        self.direction = True # T left F right
+        self.go = False
+        self.jump = False
+        self.frame_x, self.frame_y = 0, 5
+        self.width, self.height = 64, 85
+        self.velocity = 0
+        self.floor = 40
+
+    def update(self):
+        if self.go:
+            if self.direction:
+                self.x -= 7
+            else:
+                self.x += 7
+            self.frame_x = (self.frame_x + 1) % 3
+        else:
+            frame_x = 0
+
+        if self.y > self.floor:
+            self.velocity -= g
+        if self.jump:
+            self.velocity = 40
+            self.jump = False
+        elif self.y < self.floor:
+            self.y = self.floor
+            self.velocity = 0
+        self.y += self.velocity
+
+    def draw(self):
+        if self.direction:
+            self.image.clip_composite_draw(self.frame_x * self.width + int((self.frame_x + 1) * 0.5), self.frame_y * self.height, self.width, self.height, 0, 'n', self.x, self.y)
+        else:
+            self.image.clip_composite_draw(self.frame_x * self.width + int((self.frame_x + 1) * 0.5), self.frame_y * self.height, self.width, self.height, 0, 'h', self.x, self.y)
+
+
+# mario = Mario()
+#
+# open_canvas(800,600)
+#
+# clear_canvas()
+# mario.draw()
+# update_canvas()
+#
+# delay (5)
+
+# while running:
+#     clear_canvas()
+#     if go:
+#         if look:
+#             x -= 5
+#         else:
+#             x += 5
+#         frame_x = (frame_x + 1) % 3
+#     else:
+#         frame_x = 0
+#     if y > 40:
+#         down_vector -= g
+#     if jump:
+#         down_vector = 40
+#         jump = False
+#     elif y <= 40:
+#         y = 40
+#         down_vector = 0
+#     y += down_vector
+#     mario.clip_draw(frame_x * width + int((frame_x + 1) * 0.5), frame_y * height, width, height, x, y)
+#     # mario.clip_composite_draw(frame_x * width + int((frame_x + 1) * 0.5), frame_y * height, width, height, math.radians(0), True, x, y)
+#     update_canvas()
+#     delay(0.05)
+#     handle_events()
+#
+# close_canvas()
