e833f48be51245df0f8b740a51540a082984801f
niche0905 <yudle537@gmail.com>
2021-11-07 20:08:03 +0900

    프레임 워크 추가와 나눌 스테이트 생성

---
+++ main_state.py
@@ -0,0 +1,43 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+
+
+name = "MainState"
+
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    pass
+
+
+
+def update():
+    pass
+
+def draw():
+    pass
+
+
+
+
+
---
+++ mygame.py
@@ -0,0 +1,9 @@
+import game_framework
+import pico2d
+
+# 추가할 스테이트 임포트
+import main_state
+
+pico2d.open_canvas()
+game_framework.run(main_state)
+pico2d.close_canvas()
